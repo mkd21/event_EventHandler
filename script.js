1cdc317ef3d00dd109737b567d684ed2bfd4945d
Mayank Deep <mayankdeep217@gmail.com>
2024-02-07 23:06:47 +0530

    added files

---
+++ script.js
@@ -0,0 +1,57 @@
+        // EVENT HANDLERS 
+
+
+            // way 1
+// let clickProperty = document.getElementById("first");
+
+// clickProperty.addEventListener("click" , ()=> {
+//     console.log("clicked");
+// });
+
+//                 // way 2  adding attribute in HTML direclty and accessing as event
+
+function directExecutor()
+{
+    console.log("Hovered on me");
+}
+
+//         // way 3 
+
+// clickProperty.onclick = function exec(){
+//     console.log("Type 3 of execution");
+// }
+
+
+
+
+
+            // if we want to access the element by class then we will need to mention the index because class in not unique and hence it will return in array with all the elements with that class 
+
+// so we need to select any one element to execute the event 
+
+
+        // way 1
+
+// let multiStorage = document.getElementsByClassName("divs")[2];
+
+// multiStorage.addEventListener("click" , () => {
+//     console.log("accessing by class");
+// });
+
+
+            // way 2 
+
+// let multiStorage = document.getElementsByClassName("divs");
+
+// multiStorage[3].addEventListener("click" , () => {
+//     console.log("accessing by class");
+// });
+
+
+// way 3 
+
+let multiStorage = document.getElementsByClassName("divs")[3];
+
+multiStorage.onclick = () => {
+    console.log("class accessing");
+}
